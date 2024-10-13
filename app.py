84aae95ee1aa24029a40e7c83fd20d07b088a1c1
maysam-asser <misoo.asser@gmail.com>
2024-10-13 23:59:53 +0300

    done

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask,request
+from db import stores,items
+app=Flask(__name__)
+
+#crud
+#read all
+@app.route('/stores',methods=['GET'])
+def get_all_stores():
+    return list(stores.values())
+
+@app.route('/items',methods=['GET'])  
+def get_all_items():
+    return list(items.values())  
+
+#read specific store
+@app.route('/stores/<int:store_id>',methods=['GET'])
+def get_store(store_id):
+    store=stores.get(store_id)
+    if store:
+        return store,200
+    return {'message': f'store with id{store_id}not found'},404  
+
+#read specific item
+@app.route('/items/<int:item_id>',methods=['GET'])
+def get_item(item_id):
+    item=items.get(item_id)
+    if item:
+        return item,200
+    return {'message': f'item with id{item_id}not found'},404    
+
+#create new store
+@app.route('/stores/add',methods=['POST'])  
+def add_new_store():
+    new_store_data=dict(request.get_json())
+    store_id=max(list(stores.keys()))+1 if len(list(stores.keys()))>0 else 1
+    stores[store_id]=new_store_data
+    return {'message':'added succ.'},201
+
+#create new item
+@app.route('/items/add',methods=['POST'])  
+def add_new_item():
+    new_item_data=dict(request.get_json())
+    item_id=max(list(items.keys()))+1
+    items[item_id]=new_item_data
+    return 201
+
+#delete item
+@app.route('/items/delete/<int:item_id>',methods=['DELETE'])  
+def delete_item(item_id):
+    try:
+        del items[item_id]
+    except:
+        return{'message':'Item not Found'},404
+    return{'message':'Deleted successfully'}, 204   
+
+#delete store
+@app.route('/stores/delete/<int:store_id>',methods=['DELETE'])  
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+    except:
+        return{'message':'Store not Found'},404
+    return{'message':'Deleted successfully'}, 204 
+
+#update store
+@app.route('/stores/update/<int:store_id>',methods=['PUT'])  
+def update_store(store_id):
+    updated_store_data=dict(request.get_json())
+    try:
+        stores[store_id]=updated_store_data
+    except:
+        return{'message':'Store not Found'},404
+
+    return {'message':'updated succ.'},201
+
---
+++ db.py
@@ -0,0 +1,17 @@
+stores={
+    # 1:{
+    #     'name':'store 1',
+    #     'location':'october',
+    #     'phone number':1234
+
+    # }
+}
+
+
+items={
+    # 1:{'price':1_000_000,
+    # 'color':'red',
+    # 'size':'M',
+    # 'store_id':1
+    # }
+}
