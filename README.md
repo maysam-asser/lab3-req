58218f5ad779989633d0badc95ab9c82a2467e5d
maysam-asser <misoo.asser@gmail.com>
2024-10-14 22:14:57 +0300

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,117 @@
-# lab3-req
+# store app
+
+## Overview
+A REST API for managing stores and items as it supports CRUD operations.
+
+## Features
+* Get all stores
+* Get a specific store
+* Add a new store
+* Update a store
+* Delete a store
+* Get all items
+* Get a specific item
+* Add a new item
+* Delete an item
+
+## Dependencies
+- Python 
+- Setting up the virtualenv
+- Flask
+
+## Setup Instructions
+1. Clone the repository:
+
+```bash
+git clone https://github.com/maysam-asser/login-app
+```
+
+2. Set up a virtual environment:
+
+```bash
+virtualenv venv
+venv/Scripts/activate       
+```
+1. Install dependencies:
+
+```bash
+pip install Flask
+```
+
+4. Run the application:
+    
+```bash
+flask run
+```
+
+5. Access the application: Open your browser and navigate to http://127.0.0.1:5000.
+
+
+
+## API Endpoints
+### Stores
+* Get all stores: `GET /stores`
+* Get a specific `store: GET /stores/<int:store_id>`
+* Add a new store: `POST /stores/add`
+* Update a store: `PUT /stores/update/<int:store_id>`
+* Delete a store: `DELETE /stores/delete/<int:store_id>`
+
+### Items
+* Get all items: `GET /items`
+* Get a specific item: `GET /items/<int:item_id>`
+* Add a new item: `POST /items/add`
+* Delete an item: `DELETE /items/delete/<int:item_id>`
+
+
+## Postman
+<a href="https://web.postman.co/workspace/abe4d4be-76b7-461b-822e-a13d4b89ab87/overview">postman collection url</a>
+
+## Swagger
+
+<img src="scr\Screenshot 2024-10-14 210305.png" width="400"/>
+
+### Items:
+
+* Get all items: `GET /items`
+
+<img src="scr\Screenshot 2024-10-14 211124.png" width="400"/>
+
+* Add a new item: `POST /items/add`
+
+<img src="scr\Screenshot 2024-10-14 211156.png" width="400"/>
+
+<img src="scr\Screenshot 2024-10-14 211219.png" width="400"/>
+
+### Stores:
+
+* Get all stores: `GET /stores`
+
+<img src="scr\Screenshot 2024-10-14 211235.png" width="400"/>
+
+* Get a specific `store: GET /stores/<int:store_id>`
+
+<img src="scr\Screenshot 2024-10-14 211302.png" width="400"/>
+
+<img src="scr\Screenshot 2024-10-14 211323.png" width="400"/>
+
+* Add a new store: `POST /stores/add`
+
+<img src="scr\Screenshot 2024-10-14 211400.png" width="400"/>
+
+<img src="scr\Screenshot 2024-10-14 211414.png" width="400"/>
+
+* Delete a store: `DELETE /stores/delete/<int:store_id>`
+
+<img src="scr\Screenshot 2024-10-14 211429.png" width="400"/>
+
+<img src="scr\Screenshot 2024-10-14 211443.png" width="400"/>
+
+* Update a store: `PUT /stores/update/<int:store_id>`
+
+<img src="scr\Screenshot 2024-10-14 220625.png" width="400"/>
+
+<img src="scr\Screenshot 2024-10-14 220648.png" width="400"/>
+
+
+
+
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,request
 from db import stores,items
+import uuid 
 app=Flask(__name__)
 
 #crud
@@ -13,7 +14,7 @@ def get_all_items():
     return list(items.values())  
 
 #read specific store
-@app.route('/stores/<int:store_id>',methods=['GET'])
+@app.route('/stores/<string:store_id>',methods=['GET'])
 def get_store(store_id):
     store=stores.get(store_id)
     if store:
@@ -21,7 +22,7 @@ def get_store(store_id):
     return {'message': f'store with id{store_id}not found'},404  
 
 #read specific item
-@app.route('/items/<int:item_id>',methods=['GET'])
+@app.route('/items/<string:item_id>',methods=['GET'])
 def get_item(item_id):
     item=items.get(item_id)
     if item:
@@ -32,7 +33,9 @@ def get_item(item_id):
 @app.route('/stores/add',methods=['POST'])  
 def add_new_store():
     new_store_data=dict(request.get_json())
-    store_id=max(list(stores.keys()))+1 if len(list(stores.keys()))>0 else 1
+    # store_id=max(list(stores.keys()))+1 if len(list(stores.keys()))>0 else 1
+    store_id=uuid.uuid4().hex
+    print(store_id)
     stores[store_id]=new_store_data
     return {'message':'added succ.'},201
 
@@ -40,9 +43,14 @@ def add_new_store():
 @app.route('/items/add',methods=['POST'])  
 def add_new_item():
     new_item_data=dict(request.get_json())
-    item_id=max(list(items.keys()))+1
+    if not stores.get(new_item_data['store_id']):
+        return {'message':'store not found to add the item in it'},400
+
+    item_id=uuid.uuid4().hex
+    print(item_id)
     items[item_id]=new_item_data
-    return 201
+    return {'message':'added succ.'},201
+
 
 #delete item
 @app.route('/items/delete/<int:item_id>',methods=['DELETE'])  
@@ -54,7 +62,7 @@ def delete_item(item_id):
     return{'message':'Deleted successfully'}, 204   
 
 #delete store
-@app.route('/stores/delete/<int:store_id>',methods=['DELETE'])  
+@app.route('/stores/delete/<string:store_id>',methods=['DELETE'])  
 def delete_store(store_id):
     try:
         del stores[store_id]
@@ -63,13 +71,20 @@ def delete_store(store_id):
     return{'message':'Deleted successfully'}, 204 
 
 #update store
-@app.route('/stores/update/<int:store_id>',methods=['PUT'])  
+@app.route('/stores/update/<string:store_id>',methods=['PUT'])  
 def update_store(store_id):
     updated_store_data=dict(request.get_json())
-    try:
-        stores[store_id]=updated_store_data
-    except:
-        return{'message':'Store not Found'},404
+    if not updated_store_data:
+        return{'message':'no updated info.'}, 400
+    targeted_store=stores.get(store_id)
+    if targeted_store:
+        for key in updated_store_data.keys():
+            stores[store_id][key]=updated_store_data[key]
 
-    return {'message':'updated succ.'},201
+
+        return {'message':'updated succ.'},201
+
+
+    return{'message':'Store not Found'},404
+
 
